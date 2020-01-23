2e99f965f8869c18598b1b78b0a190fa07c94c1a
enif.lee <enif.lee@gmail.com>
2020-01-24 05:02:24 +0900

    Fix missing trailer header  (#4)
    * fix #3 right way that throw RpcException with removing ObjectCreator
    
    * add trailing header test for invalid request

--- src/Grpc.AspNetCore.FluentValidation.Test/Integration/CustomValidatorIntegrationTest.cs
+++ src/Grpc.AspNetCore.FluentValidation.Test/Integration/CustomValidatorIntegrationTest.cs
@@ -1,3 +1,5 @@
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentValidation;
 using Grpc.AspNetCore.FluentValidation.SampleRpc;
@@ -53,6 +55,35 @@ namespace Grpc.AspNetCore.FluentValidation.Test.Integration
             var rpcException = await Assert.ThrowsAsync<RpcException>(Action);
             Assert.Equal(StatusCode.InvalidArgument, rpcException.Status.StatusCode);
         }
+        
+        [Fact]
+        public async Task Should_ReturnWithTrailingHeader_When_RequestIsInvalid()
+        {
+            // Given
+            var spyHandler = new VerifierHeaderSpyDelegate();
+            var client = new Greeter.GreeterClient(_factory.CreateGrpcChannel(spyHandler));
+
+            // When
+            await client.SayHelloAsync(new HelloRequest {Name = string.Empty}).ResponseHeadersAsync;
+
+            // Then
+            var headers = spyHandler.ResponseMessage.Headers;
+            headers.TryGetValues("grpc-status", out var values);
+            Assert.Single(values, "3");
+            Assert.True(headers.Contains("grpc-message"));
+        }
+
+        class VerifierHeaderSpyDelegate : ResponseVersionHandler
+        {
+            public HttpResponseMessage ResponseMessage { get; set; }
+            
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                ResponseMessage = await base.SendAsync(request, cancellationToken);
+                return ResponseMessage;
+            }
+        }
+        
         public class HelloRequestValidator : AbstractValidator<HelloRequest>
         {
             public HelloRequestValidator()
