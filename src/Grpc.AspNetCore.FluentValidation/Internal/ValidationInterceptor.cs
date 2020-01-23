168d93a3a223e7d853d1ab3e5066ed272a11426c
Enif Lee <enif.lee@gmail.com>
2020-01-24 04:08:40 +0900

    fix #3, add trailer header

--- src/Grpc.AspNetCore.FluentValidation/Internal/ValidationInterceptor.cs
+++ src/Grpc.AspNetCore.FluentValidation/Internal/ValidationInterceptor.cs
@@ -9,6 +9,8 @@ namespace Grpc.AspNetCore.FluentValidation.Internal
     {
         private readonly IValidatorLocator _locator;
         private readonly IValidatorErrorMessageHandler _handler;
+        private const string StatusTrailerName = "grpc-status";
+        private const string StatusTrailerInvalidArgument = "3";
 
         public ValidationInterceptor(IValidatorLocator locator, IValidatorErrorMessageHandler handler)
         {
@@ -24,11 +26,11 @@ namespace Grpc.AspNetCore.FluentValidation.Internal
             if (_locator.TryGetValidator<TRequest>(out var validator))
             {
                 var results = await validator.ValidateAsync(request);
-
                 if (!results.IsValid)
                 {
                     var message = await _handler.HandleAsync(results.Errors);
                     context.Status = new Status(StatusCode.InvalidArgument, message);
+                    context.GetHttpContext().Response.Headers[StatusTrailerName] = StatusTrailerInvalidArgument;
                     return ObjectCreator<TResponse>.Empty;
                 }
             }
